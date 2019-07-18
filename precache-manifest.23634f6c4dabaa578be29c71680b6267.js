ac06c28e2d41f410b38db8a5e46d36acbe41a4a5
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-18 15:34:38 -0400

    Updates

---
+++ precache-manifest.23634f6c4dabaa578be29c71680b6267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "d13e72bfd0f29f27a4d3",
+    "url": "/pokedex/static/js/main.bca01902.chunk.js"
+  },
+  {
+    "revision": "31df9ff1c9e2751a122a",
+    "url": "/pokedex/static/js/2.efd09710.chunk.js"
+  },
+  {
+    "revision": "d13e72bfd0f29f27a4d3",
+    "url": "/pokedex/static/css/main.50ed3a10.chunk.css"
+  },
+  {
+    "revision": "ec424271174ccf6cd0883f3d5627ff63",
+    "url": "/pokedex/index.html"
+  }
+];
