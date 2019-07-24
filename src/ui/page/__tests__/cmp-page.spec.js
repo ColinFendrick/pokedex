b21c58b19bea35d08ddb2cb1418620904d83b5e2
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-24 08:36:49 -0400

    Update test

--- src/ui/page/__tests__/cmp-page.spec.js
+++ src/ui/page/__tests__/cmp-page.spec.js
@@ -16,38 +16,6 @@ describe('Testing the page', () => {
     expect(getByText('Loading...'))
   })
 
-  it('Has a search that works', async () => {
-    const props = {
-      collection: {
-        1: { id: '1', name: 'bulbasaur' },
-        2: { id: '2', name: 'ivysaur' },
-        3: { id: '3', name: 'venusaur' },
-        4: { id: '4', name: 'charmander' },
-        5: { id: '5', name: 'charmeleon' },
-        6: { id: '6', name: 'charizard' },
-        7: { id: '7', name: 'squirtle' },
-        8: { id: '8', name: 'wartortle' },
-        9: { id: '9', name: 'blastoise' }
-      },
-      getPokemons: () => Promise.resolve({}),
-      isFetched: false
-    }
-
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Page {...props} />
-    )
-
-    const searchBar = getByPlaceholderText('Enter pokemon name...')
-    await waitForElement(() => [
-      getByText('bulbasaur'),
-      getByText('charmander'),
-      getByText('squirtle')
-    ])
-    fireEvent.change(searchBar, { target: { value: 'saur' } })
-    expect(queryByText('charmander')).toBeNull()
-    expect(queryByText('squirtle')).toBeNull()
-  })
-
   it('Has an error and no other stuff if an error exists', async () => {
     const props = {
       getPokemons: () =>
@@ -63,10 +31,51 @@ describe('Testing the page', () => {
     await waitForElement(() => getByText('Sorry, no pokemons'))
   })
 
+  it('Has a search that works', async () => {
+    const props = {
+      getPokemons: () => Promise.resolve({}),
+      collection: {
+        1: { id: '1', name: 'bulbasaur' },
+        2: { id: '2', name: 'ivysaur' },
+        3: { id: '3', name: 'venusaur' },
+        4: { id: '4', name: 'charmander' },
+        5: { id: '5', name: 'charmeleon' },
+        6: { id: '6', name: 'charizard' },
+        7: { id: '7', name: 'squirtle' },
+        8: { id: '8', name: 'wartortle' },
+        9: { id: '9', name: 'blastoise' }
+      },
+      isFetched: false
+    }
+
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Page {...props} />
+    )
+
+    const searchBar = getByPlaceholderText('Enter pokemon name...')
+    await waitForElement(() => [
+      getByText('bulbasaur'),
+      getByText('charmander'),
+      getByText('squirtle')
+    ])
+    fireEvent.change(searchBar, { target: { value: 'saur' } })
+    expect(getByText('bulbasaur'))
+    expect(queryByText('charmander')).toBeNull()
+    expect(queryByText('squirtle')).toBeNull()
+  })
+
   it('Has a matching snapshot', () => {
     const props = {
       collection: {
-        1: { id: '1', name: 'bulbasaur' }
+        1: { id: '1', name: 'bulbasaur' },
+        2: { id: '2', name: 'ivysaur' },
+        3: { id: '3', name: 'venusaur' },
+        4: { id: '4', name: 'charmander' },
+        5: { id: '5', name: 'charmeleon' },
+        6: { id: '6', name: 'charizard' },
+        7: { id: '7', name: 'squirtle' },
+        8: { id: '8', name: 'wartortle' },
+        9: { id: '9', name: 'blastoise' }
       },
       getPokemons: () => Promise.resolve({}),
       isFetched: false
